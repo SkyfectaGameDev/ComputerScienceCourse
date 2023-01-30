b028ad381c14d3763d43272e4eb0b8c23bb4365e
SkyfectaGameDev <skyfecta@gmail.com>
2023-01-30 14:39:21 +0000

    Session 5a

---
+++ JavaScript/oop.js
@@ -0,0 +1,57 @@
+// ----- Examples of Classes -----
+class Pet
+{
+    constructor(firstname, surname, age, type, breed, colour, time)
+    {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.age = age;
+        this.type = type;
+        this.breed = breed;
+        this.colour = colour;
+        this.lastFed = time;
+    }
+    get petInfo()
+    {
+        return `${this.name} is a ${this.type}, ${this.age} year(s) old`
+    }
+    set fullName(name)
+    {
+        const array = name.split(" ");
+        this.firstname = array[0];
+        this.surname = array[1];
+    }
+    get fullName()
+    {
+        return `${this.firstname} ${this.surname}`;
+    }
+    feedPet(time)
+    {
+        this.lastFed = time;
+        return `Last feed updated to: ${this.lastFed} for ${this.name}`;
+    }
+}
+
+class PetMeds extends Pet
+{
+    constructor(firstname, surname, age, type, breed, colour, time, mtime)
+    {
+        super(firstname, surname, age, type, breed, colour, time);
+        this.lastMeds = mtime;
+    }
+    giveMeds(time)
+    {
+        this.lastMeds = time;
+        return `Last meds updated to: ${this.lastMeds} for ${this.name}`;
+    }
+}
+
+const Bella = new Pet("Bella", "", 2, "Dog", "GS", "B&T", "08:00");
+console.log(Bella.feedPet("15:00"));
+Bella.fullName = "Bella Crompton"
+console.log(Bella);
+console.log(Bella.fullName);
+const Penny = new PetMeds("Penny", "", 1, "Jumping Spider", "Phiddipus Regius", "Tan", "08:00", "14:00");
+console.log(Penny.giveMeds("12:00"));
+
+// ----- Rental Activity -----
---
+++ JavaScript/recap.js
@@ -0,0 +1,22 @@
+let firstName = "Alisha";
+console.log(firstName);
+
+let age = 21;
+console.log(age);
+
+let alishaAge = true;
+console.log(alishaAge);
+
+let fruit = ["apple", "orange", "melon"];
+console.log(fruit[0]);
+
+let myArray = [
+    ["Latte", 1],
+    ["Cappuciono", 2]
+];
+console.log(myArray[0][1]);
+
+if (firstName === "Alisha")
+{
+    
+}
