9e915a7ab4e54659260aa46801f9fe4a9f3c35a6
SkyfectaGameDev <skyfecta@gmail.com>
2023-02-21 11:36:17 +0000

    useState Counter Activity

--- React/first-app/src/App.js
+++ React/first-app/src/App.js
@@ -1,7 +1,35 @@
 import './App.css';
-import Person from './components/Person.js'
-import Gallery from './components/Gallery.js'
+import Person from './components/Person.js';
+import Gallery from './components/Gallery.js';
+import Counter from './components/Counter.js';
+import React, { useState } from 'react';
 
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  const decrease = () => {
+    if (counter > 0) {
+      setCounter(count => count - 1);
+    }
+  };
+
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+
+  return (
+    <div className="counter">
+      <h1>React Counter</h1>
+      <div className="btn__container">
+        <button className="control__btn" onClick={decrease}>-</button>
+        <span className="counter__output">{counter}</span>
+        <button className="control__btn" onClick={increase}>+</button>
+      </div>
+    </div>
+  );
+}
+
+/*
 function App() {
   return (
     <div className="container">
@@ -12,5 +40,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
